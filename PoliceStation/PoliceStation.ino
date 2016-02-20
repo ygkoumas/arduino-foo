de615d58e5a249d31fc899112f81f29cc187be85
Yannis Gkoumas <ygkoumas@gmail.com>
2016-02-20 19:07:30 +0000

    Siren effect is added

--- PoliceStation/PoliceStation.ino
+++ PoliceStation/PoliceStation.ino
@@ -1,6 +1,7 @@
 int switchState = 13;
 int k = 3;
 int l = 1;
+const float pi = 3.14;
 
 void setup(){
   pinMode(5, OUTPUT);
@@ -27,13 +28,22 @@ void loop() {
     digitalWrite(3, LOW);
     k = 4;
     l = 1;
+    int o = 100000;
 
-    for (int i = 0; i<10; i++) {
+    for (int i = 0; i<20; i++) {
       for (int i=0; i<2; i++) {
         digitalWrite(k, HIGH);
-        delay(100);
+        for (int i = 0; i<20; i++) {
+          o++;
+          tone(8, 2000+600*sin(o/100 * pi));
+          delay(8);
+        }
         digitalWrite(k,LOW);
-        delay(100);
+        for (int i = 0; i<20; i++) {
+          o++;
+          tone(8, 2000+600*sin(o/100 * pi));
+          delay(8);
+        }
       }
       digitalWrite(4, LOW);
       digitalWrite(5, LOW);
@@ -42,5 +52,6 @@ void loop() {
       if (k == 5) {l = -1;}
       k+=l;
     }
+    noTone(8);
   }
 }
