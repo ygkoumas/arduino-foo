57d0d3e79f58dff907c20c70aa8a396b293ea424
Yannis Gkoumas <ygkoumas@gmail.com>
2016-03-15 23:12:59 +0000

    Improve the sound of the siren: Make faster low-high frequency change

--- PoliceStation/Lib.cpp
+++ PoliceStation/Lib.cpp
@@ -3,10 +3,18 @@
 
 const float pi = 3.14;
 
+float sgn(float num) {
+  if (num>=0)return 1;
+  return -1;
+}
+
 float siren(int duration, float o) {
   for (int i = 0; i<duration; i++) {
     o++;
-    tone(8, 2000+600*sin(o/100 * pi));
+
+    float harmonic = sin(o/100 * pi);
+    float toneFrequency = 2000+500 * pow(abs(harmonic), 0.01) * sgn(harmonic);
+    tone(8, toneFrequency);
     Serial.println(sin(o/100 * pi));
     delay(8);
   }
